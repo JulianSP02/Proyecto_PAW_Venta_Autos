e154cafaa10a757205074a53a2c0f720e12e9f59
JJMoraC <jeffersonmoracoto@gmail.com>
2024-03-26 16:55:46 -0600

    Usuario
    Usuario model y usuario controller

--- Proyecto_Venta_Autos/Controllers/UsuarioController.cs
+++ Proyecto_Venta_Autos/Controllers/UsuarioController.cs
@@ -1,12 +1,134 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Venta_Autos.Models;
 
 namespace Proyecto_Venta_Autos.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly VentaAutosDbContext _context;
+
+        public UsuarioController(VentaAutosDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuario
         public IActionResult Index()
+        {
+            return View(_context.Usuarios.ToList());
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuarios.FirstOrDefault(m => m.ID == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,Nombre,Email,Contraseña,Rol")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("ID,Nombre,Email,Contraseña,Rol")] Usuario usuario)
+        {
+            if (id != usuario.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuarios.FirstOrDefault(m => m.ID == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.ID == id);
+        }
+
     }
 }
---
+++ Proyecto_Venta_Autos/Models/VentaAutosDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_Venta_Autos.Models
+{
+    public class VentaAutosDbContext : DbContext
+    {
+        public VentaAutosDbContext(DbContextOptions<VentaAutosDbContext> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Nombre).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Contraseña).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Rol).IsRequired().HasMaxLength(20);
+
+                entity.HasCheckConstraint("CK_Rol", "[Rol] IN ('Cliente', 'Administrador')");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
