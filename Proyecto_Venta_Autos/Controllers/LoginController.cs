cbaad14b4e21b1b856cb586c3250f6ed0a2a3e14
JJMoraC <jeffersonmoracoto@gmail.com>
2024-04-07 13:34:14 -0600

    Login Controller

---
+++ Proyecto_Venta_Autos/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Venta_Autos.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Proyecto_Venta_Autos.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoginController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7138/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/Login", usuario);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Credenciales inválidas");
+                    return View(usuario);
+                }
+            }
+            else
+            {
+                return View(usuario);
+            }
+        }
+    }
+}
